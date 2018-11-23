c6ccf449adf931d8ed948a28edeee22129115e93
Felix Kutzner <felixkutzner@gmail.com>
2018-11-23 23:16:06 +0100

    add and-gate-constraint encoder

--- cscl/basic_gate_encoders.py
+++ cscl/basic_gate_encoders.py
@@ -22,3 +22,27 @@ def encode_or_gate(clause_consumer: ClauseConsumer, input_lits, output_lit = Non
         clause_consumer.consume_clause([-lit, output_lit])
 
     return output_lit
+
+
+def encode_and_gate(clause_consumer: ClauseConsumer, input_lits, output_lit = None):
+    """
+    Creates an AND gate.
+
+    :param clause_consumer: The clause consumer to which the clauses of the gate encoding shall be added.
+    :param input_lits: The gate's input literals.
+    :param output_lit: The gate's output literal. If output_lit is None, a positive literal with a
+                       new variable will be used as the gate's output literal.
+    :return: The encoded gate's output literal.
+    """
+
+    if output_lit is None:
+        output_lit = clause_consumer.create_variable()
+
+    fwd_clause = list(map(lambda x: -x, input_lits))
+    fwd_clause.append(output_lit)
+    clause_consumer.consume_clause(fwd_clause)
+
+    for lit in input_lits:
+        clause_consumer.consume_clause([lit, -output_lit])
+
+    return output_lit
--- tests/cscl/test_basic_gate_encoders.py
+++ tests/cscl/test_basic_gate_encoders.py
@@ -1,28 +1,34 @@
 from unittest import TestCase
-
-from cscl.basic_gate_encoders import encode_or_gate
-
+from cscl.basic_gate_encoders import *
 from tests.testutils.trivial_sat_solver import TrivialSATSolver
+
+
+def encoder_returns_output_literal(encoder_fn):
+    checker = TrivialSATSolver()
+    variables = []
+    for i in range(0, 10):
+        variables.append(checker.create_variable())
+
+    result = encoder_fn(checker, [variables[0]], variables[1])
+    return result == variables[1]
+
+
+def encoder_returns_new_output_literal_by_default(encoder_fn):
+    checker = TrivialSATSolver()
+    variables = []
+    for i in range(0, 10):
+        variables.append(checker.create_variable())
+
+    result = encode_or_gate(checker, [variables[0]])
+    return not result in variables and not -result in variables
 
 
 class TestEncodeOrGate(TestCase):
     def test_encode_or_gate_returns_output_literal(self):
-        checker = TrivialSATSolver()
-        vars = []
-        for i in range(0, 10):
-            vars.append(checker.create_variable())
-
-        result = encode_or_gate(checker, [vars[0]], vars[1])
-        assert(result == vars[1])
+        assert(encoder_returns_output_literal(encode_or_gate))
 
     def test_encode_or_gate_returns_new_output_literal_by_default(self):
-        checker = TrivialSATSolver()
-        vars = []
-        for i in range(0, 10):
-            vars.append(checker.create_variable())
-
-        result = encode_or_gate(checker, [vars[0]])
-        assert (not result in vars and not -result in vars)
+        assert(encoder_returns_new_output_literal_by_default(encode_or_gate))
 
     def encode_or_gate_n_ary_test_full(self, n):
         """
@@ -68,3 +74,57 @@ class TestEncodeOrGate(TestCase):
 
     def test_encode_or_gate_create_quaternary_or_gate(self):
         self.encode_or_gate_n_ary_test_full(4)
+
+
+class TestEncodeAndGate(TestCase):
+    def test_encode_and_gate_returns_output_literal(self):
+        assert(encoder_returns_output_literal(encode_and_gate))
+
+    def test_encode_and_gate_returns_new_output_literal_by_default(self):
+        assert(encoder_returns_new_output_literal_by_default(encode_and_gate))
+
+    def encode_and_gate_n_ary_test_full(self, n):
+        """
+        Tests (via assert()) encode_and_gate for all combinations of outputs and n inputs.
+
+        :param n: The non-negative number of inputs
+        :return: None
+        """
+
+        checker = TrivialSATSolver()
+        inputs = []
+        for i in range(0, n):
+            inputs.append(checker.create_variable())
+
+        output = encode_and_gate(checker, inputs)
+
+        for i in range(1, 2**n):
+            assumptions = inputs[:]
+            for j in range(0, n):
+                if (i & (1 << j)) != 0:
+                    assumptions[j] = -assumptions[j]
+            assumptions.append(-output)
+            assert (checker.solve(assumptions) is True)
+            assumptions[n] = -assumptions[n]
+            assert (checker.solve(assumptions) is False)
+
+        assumptions = inputs[:]
+        assumptions.append(output)
+        assert (checker.solve(assumptions) is True)
+        assumptions[n] = -assumptions[n]
+        assert (checker.solve(assumptions) is False)
+
+    def test_encode_and_gate_create_nullary_and_gate(self):
+        self.encode_and_gate_n_ary_test_full(0)
+
+    def test_encode_and_gate_create_unary_and_gate(self):
+        self.encode_and_gate_n_ary_test_full(1)
+
+    def test_encode_and_gate_create_binary_and_gate(self):
+        self.encode_and_gate_n_ary_test_full(2)
+
+    def test_encode_and_gate_create_ternary_and_gate(self):
+        self.encode_and_gate_n_ary_test_full(3)
+
+    def test_encode_and_gate_create_quaternary_and_gate(self):
+        self.encode_and_gate_n_ary_test_full(4)
