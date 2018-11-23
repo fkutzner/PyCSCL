5ef3bb0f792eca497c155b1cca3d12d1146ddfd7
Felix Kutzner <felixkutzner@gmail.com>
2018-11-23 23:44:33 +0100

    add DIMACS-printing ClauseConsumer

---
+++ cscl/dimacs_printer.py
@@ -0,0 +1,30 @@
+from cscl.clause_consumer import ClauseConsumer
+
+
+class DIMACSPrinter(ClauseConsumer):
+    """
+    A ClauseConsumer printing the consumed clauses in DIMACS format
+    """
+
+    def __init__(self):
+        self.clauses = []
+        self.num_vars = 0
+
+    def consume_clause(self, clause):
+        self.clauses.append(clause)
+
+    def create_variable(self):
+        self.num_vars += 1
+        return self.num_vars
+
+    def print(self, line_print_fn):
+        """
+        Prints the collected clauses using the given printing function.
+
+        :param line_print_fn: A function accepting a string as its sole argument. The DIMACS output
+                              is passed line-by-line to line_print_fn.
+        :return: None
+        """
+        line_print_fn("p cnf " + str(self.num_vars) + " " + str(len(self.clauses)))
+        for clause in self.clauses:
+            line_print_fn((' '.join(map(str, clause))) + " 0")
