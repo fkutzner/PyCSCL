5cbe124278a899c5a0e7b19fc4eaadfc091f870d
Felix Kutzner <felixkutzner@gmail.com>
2018-11-23 20:30:40 +0100

    add a logging decorator for ClauseConsumer

---
+++ tests/testutils/logging_clause_consumer_decorator.py
@@ -0,0 +1,35 @@
+from cscl.clause_consumer import ClauseConsumer
+
+
+class LoggingClauseConsumerDecorator(ClauseConsumer):
+    """
+    A ClauseConsumer that logs the collected clauses.
+    """
+
+    def __init__(self, decorated_clause_consumer: ClauseConsumer):
+        """
+        Constructs a LoggingClauseConsumerDecorator object.
+
+        :param decorated_clause_consumer: The ClauseConsumer to be decorated.
+        """
+        self.decorated_clause_consumer = decorated_clause_consumer
+        self.clauses = []
+
+    def consume_clause(self, clause):
+        self.clauses.append(clause)
+        self.decorated_clause_consumer.consume_clause(clause)
+
+    def create_variable(self):
+        return self.decorated_clause_consumer.create_variable()
+
+    def to_string(self):
+        """
+        Returns a string representation of the clauses added via consume_clause(),
+        preserving their order of consumption.
+
+        :return: a string as described above.
+        """
+        result = ""
+        for clause in self.clauses:
+            result += str(clause) + "\n"
+        return result
