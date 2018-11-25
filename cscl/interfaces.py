3447860ec776758e095dd28371eec3677ee6cb32
Felix Kutzner <felixkutzner@gmail.com>
2018-11-25 12:17:31 +0100

    add a SatSolver interface

--- cscl/interfaces.py
+++ cscl/interfaces.py
@@ -28,3 +28,33 @@ class ClauseConsumer(abc.ABC):
         :return: None
         """
         pass
+
+
+class SatSolver(CNFVariableFactory):
+    """A role interface for SAT solvers."""
+
+    @abc.abstractmethod
+    def solve(self, assumptions: list):
+        """
+        Determines whether the problem is satisfiable.
+
+        :param assumptions: A list of literals whose assignment shall be forced to true during the solving process.
+                            For each literal l in assumptions, either l or -l must have been produced via
+                            create_variable().
+        :return: True if the problem is satisfiable; False if the problem is not satisfiable; None if the solver
+                 did not reach a conclusion (e.g. due to a timeout).
+        """
+        pass
+
+    @abc.abstractmethod
+    def get_assignment(self, lit):
+        """
+        Gets the assignment of the given literal within the model produced by the previous call to solve().
+
+        This method may only be called when the previous call to solve() returned True.
+
+        :param lit: A literal l such that l or -l has been produced via create_variable.
+        :return: True if lit has a positive assignment, False if lit has a negative assignment, None if lit
+                 has no assignment.
+        """
+        pass
