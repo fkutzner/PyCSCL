fcbb4c7c5b1f3dd359d84214742c275380185075
Felix Kutzner <felixkutzner@gmail.com>
2018-12-08 17:02:12 +0100

    add a sort parser for smt_qfbv_solver

--- examples/smt_qfbv_solver/smtlib2_parser.py
+++ examples/smt_qfbv_solver/smtlib2_parser.py
@@ -33,3 +33,22 @@ def parse_smtlib2_literal(lit_string: str, sort_ctx: sorts.SortContext) -> Union
         # not supported
         raise ValueError("String literals are not supported")
     return None
+
+
+def parse_smtlib2_sort(parsed_sexp, sort_ctx: sorts.SortContext):
+    """
+    Parses an SMTLib2-formatted sort expression.
+
+    :param parsed_sexp: The sort's s-expression, e.g. "Int" or ["_", "BitVec", "32"].
+    :param sort_ctx: A sort context.
+    :return: The sort corresponding to parsed_sexp.
+    :raises ValueError if parsed_sexp is not a well-formed sort s-expression, or if the sort is unsupported.
+    """
+    if parsed_sexp == "Int":
+        return sort_ctx.get_int_sort()
+    elif len(parsed_sexp) == 3 and parsed_sexp[0:2] == ["_", "BitVec"]:
+        length_str = parsed_sexp[2]
+        if not length_str.isnumeric() or '.' in length_str:
+            raise ValueError("Illegal BitVec type length in " + str(parsed_sexp))
+        return sort_ctx.get_bv_sort(int(length_str))
+    raise ValueError("Unsupported sort " + str(parsed_sexp))
--- tests/examples/smt_qfbv_solver/test_smtlib2_parser.py
+++ tests/examples/smt_qfbv_solver/test_smtlib2_parser.py
@@ -52,3 +52,38 @@ class TestParseSmtlib2Literal(unittest.TestCase):
         assert result.get_literal() is 21, "Unexpected result " + str(result)
         assert isinstance(result.get_sort(), sorts.BitvectorSort)
         assert result.get_sort().get_len() == 5
+
+
+class TestParseSmtlib2Sort(unittest.TestCase):
+    def test_parses_int_sort(self):
+        sort_ctx = sorts.SortContext()
+        result = smt.parse_smtlib2_sort("Int", sort_ctx)
+        assert isinstance(result, sorts.IntegerSort)
+
+    def test_parses_bv1_sort(self):
+        sort_ctx = sorts.SortContext()
+        result = smt.parse_smtlib2_sort(["_", "BitVec", "1"], sort_ctx)
+        assert isinstance(result, sorts.BitvectorSort)
+        assert result.get_len() == 1
+
+    def test_parses_bv13_sort(self):
+        sort_ctx = sorts.SortContext()
+        result = smt.parse_smtlib2_sort(["_", "BitVec", "13"], sort_ctx)
+        assert isinstance(result, sorts.BitvectorSort)
+        assert result.get_len() == 13
+
+    def test_refuses_malformed_bv_sort(self):
+        sort_ctx = sorts.SortContext()
+        with self.assertRaises(ValueError):
+            smt.parse_smtlib2_sort(["_", "BitVec"], sort_ctx)
+        with self.assertRaises(ValueError):
+            smt.parse_smtlib2_sort(["_", "BitVec", "a"], sort_ctx)
+        with self.assertRaises(ValueError):
+            smt.parse_smtlib2_sort(["_", "BitVec", "-1"], sort_ctx)
+
+    def test_refuses_unknown_sort(self):
+        sort_ctx = sorts.SortContext()
+        with self.assertRaises(ValueError):
+            smt.parse_smtlib2_sort("Foo", sort_ctx)
+        with self.assertRaises(ValueError):
+            smt.parse_smtlib2_sort(["_", "Foo"], sort_ctx)
