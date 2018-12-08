96c4c0092b8283f1933dba3837d4e221c6024fe2
Felix Kutzner <felixkutzner@gmail.com>
2018-12-08 17:29:59 +0100

    add function signature and scoping-support classes for smt_qfbv_solver

--- examples/smt_qfbv_solver/smtlib2_parser.py
+++ examples/smt_qfbv_solver/smtlib2_parser.py
@@ -52,3 +52,117 @@ def parse_smtlib2_sort(parsed_sexp, sort_ctx: sorts.SortContext):
             raise ValueError("Illegal BitVec type length in " + str(parsed_sexp))
         return sort_ctx.get_bv_sort(int(length_str))
     raise ValueError("Unsupported sort " + str(parsed_sexp))
+
+
+class FunctionSignature:
+    """
+    A function signature.
+    """
+
+    def __init__(self, domain_sorts_to_range_sort_fn, arity: int, is_shadowable: bool):
+        """
+        Initializes the FunctionSignature object.
+
+        :param domain_sorts_to_range_sort_fn: The function determining the represented function's signature.
+                                              For Sort objects s1, ..., sN, domain_sorts_to_range_sort_fn((s1, ..., sN))
+                                              returns the function's range sort for parameter sorts s1, ..., sN;
+                                              If s1, ..., sN is not part of the function's domain, None is returned.
+        :param arity: The function's arity.
+        :param is_shadowable: True iff the function may be shadowed and may shadow other functions; False otherwise.
+        """
+        self.__dtr_fun = domain_sorts_to_range_sort_fn
+        self.__arity = arity
+        self.__is_shadowable = is_shadowable
+
+    def get_range_sort(self, domain_sorts):
+        """
+        Gets the function's range sort for domain sorts s1, ..., sN.
+
+        :param domain_sorts: The query's domain sorts.
+        :return: The corresponding range sort, or None domain_sorts is not part of the function's domain.
+        """
+        return self.__dtr_fun(domain_sorts)
+
+    def get_arity(self):
+        """
+        Gets the function's arity.
+
+        :return: The function's arity.
+        """
+        return self.__arity
+
+    def is_shadowable(self):
+        """
+        Returns True iff the function may be shadowed and may shadow other function declarations.
+
+        :return: True iff the function may be shadowed and may shadow other function declarations.
+        """
+        return self.__is_shadowable
+
+
+class SyntacticFunctionScope:
+    """
+    A scope of function declarations.
+    """
+
+    def __init__(self, parent_scope):
+        """
+        Initializes the SyntacticFunctionScope object.
+
+        :param parent_scope: The scope's parent scope. If the scope has no parent scope, this argument must be None.
+        """
+        self.__parent = parent_scope
+        self.__signatures = dict()
+
+    def get_signature(self, func_name: str) -> Union[FunctionSignature, type(None)]:
+        """
+        Gets the function signature for a function.
+
+        If this scope does not contain a matching function signature and this scope has
+        a parent scope, the parent scope is queried for a function signature for func_name.
+
+        :param func_name: The function's name.
+        :return: The function's signature. If the scope has no function with name func_name, None is returned instead.
+        """
+        if func_name in self.__signatures.keys():
+            return self.__signatures[func_name]
+        elif self.__parent is not None:
+            return self.__parent.get_signature(func_name)
+        else:
+            return None
+
+    def add_signature(self, func_name: str, signature: FunctionSignature):
+        """
+        Adds a function signature to the scope.
+
+        :param func_name: The function's name.
+        :param signature: The function's signature.
+        :return: None
+        :raises ValueError if adding a function named func_name is prevented by the existence of a same-named,
+                           unshadowable function in this scope.
+        """
+        if self.has_unshadowable_signature(func_name):
+            raise ValueError("Function " + func_name + " cannot be redefined or shadowed")
+        self.__signatures[func_name] = signature
+
+    def has_unshadowable_signature(self, func_name):
+        """
+        Determines whether the given function name is associated with an unshadowable function.
+
+        :param func_name: The function's name.
+        :return: True iff the given function name is associated with an unshadowable function.
+        """
+        func_signature = self.get_signature(func_name)
+        if func_signature is None:
+            return False
+        else:
+            return not func_signature.is_shadowable()
+
+    def set_parent(self, new_parent):
+        """
+        Sets the scope's parent scope.
+
+        :param new_parent: The new parent scope.
+        :return: None
+        """
+        self.__parent = new_parent
--- tests/examples/smt_qfbv_solver/test_smtlib2_parser.py
+++ tests/examples/smt_qfbv_solver/test_smtlib2_parser.py
@@ -87,3 +87,43 @@ class TestParseSmtlib2Sort(unittest.TestCase):
             smt.parse_smtlib2_sort("Foo", sort_ctx)
         with self.assertRaises(ValueError):
             smt.parse_smtlib2_sort(["_", "Foo"], sort_ctx)
+
+
+class TestSyntacticFunctionScope(unittest.TestCase):
+    def test_has_added_signature(self):
+        sort_ctx = sorts.SortContext()
+        sig = smt.FunctionSignature(lambda x: sort_ctx.get_int_sort() if x == [sort_ctx.get_bv_sort(2)] else None,
+                                    1, True)
+        under_test = smt.SyntacticFunctionScope(None)
+        under_test.add_signature("foo", sig)
+        assert(under_test.get_signature("foo") is sig)
+
+    def test_queries_parent_scope(self):
+        sort_ctx = sorts.SortContext()
+        sig = smt.FunctionSignature(lambda x: sort_ctx.get_int_sort() if x == [sort_ctx.get_bv_sort(2)] else None,
+                                    1, True)
+        parent = smt.SyntacticFunctionScope(None)
+        under_test = smt.SyntacticFunctionScope(parent)
+        parent.add_signature("foo", sig)
+        assert(under_test.get_signature("foo") is sig)
+
+    def test_set_parent_scope(self):
+        sort_ctx = sorts.SortContext()
+        sig = smt.FunctionSignature(lambda x: sort_ctx.get_int_sort() if x == [sort_ctx.get_bv_sort(2)] else None,
+                                    1, True)
+        parent = smt.SyntacticFunctionScope(None)
+        under_test = smt.SyntacticFunctionScope(None)
+        under_test.set_parent(parent)
+        parent.add_signature("foo", sig)
+        assert(under_test.get_signature("foo") is sig)
+
+    def test_refuses_to_add_when_unshadowable(self):
+        sort_ctx = sorts.SortContext()
+        sig = smt.FunctionSignature(lambda x: sort_ctx.get_int_sort() if x == [sort_ctx.get_bv_sort(2)] else None,
+                                    1, False)
+        parent = smt.SyntacticFunctionScope(None)
+        parent.add_signature("foo", sig)
+
+        under_test = smt.SyntacticFunctionScope(parent)
+        with self.assertRaises(ValueError):
+            under_test.add_signature("foo", sig)
