a14fe9b09db36a6c2fd26dc26abc6d100e5d6625
Felix Kutzner <felixkutzner@gmail.com>
2018-12-15 21:46:06 +0100

    add fixed-size bitvector theory scope factory to smt_qfbv_solver

--- examples/smt_qfbv_solver/theories.py
+++ examples/smt_qfbv_solver/theories.py
@@ -86,3 +86,66 @@ class CoreSyntacticFunctionScopeFactory(TheorySyntacticFunctionScopeFactory):
         self.__add_binary_fns(result, sort_ctx)
         self.__add_constants(result, sort_ctx)
         return result
+
+
+class FixedSizeBVSyntacticFunctionScopeFactory(TheorySyntacticFunctionScopeFactory):
+    """
+    A SyntacticFunctionScopeFactory for the FixedSizeBitVectors theory.
+
+    See http://smtlib.cs.uiowa.edu/theories-FixedSizeBitVectors.shtml
+    """
+
+    @staticmethod
+    def __add_concat_fn(target: synscope.SyntacticFunctionScope, sort_ctx: sorts.SortContext):
+        def __concat_sig_fn(x):
+            if len(x) == 2 and all(isinstance(z, sorts.BitvectorSort) for z in x):
+                return sort_ctx.get_bv_sort(x[0].get_len() + x[1].get_len())
+        target.add_signature("concat", synscope.FunctionSignature(__concat_sig_fn, 2, False))
+
+    @staticmethod
+    def __add_extract_fn(target: synscope.SyntacticFunctionScope, sort_ctx: sorts.SortContext):
+        def __extract_sig_fn(x):
+            if len(x) == 3 and type(x[0]) is int and type(x[1]) is int and isinstance(x[2], sorts.BitvectorSort):
+                i, j = x[0:2]
+                if (x[2].get_len() > i) and (i >= j) and (j >= 0):
+                    return sort_ctx.get_bv_sort(i - j + 1)
+        target.add_signature(synscope.SyntacticFunctionScope.mangle_parametrized_function_name("extract"),
+                             synscope.FunctionSignature(__extract_sig_fn, 1, False, 2))
+
+    @staticmethod
+    def __add_bv_neg_fns(target: synscope.SyntacticFunctionScope, sort_ctx: sorts.SortContext):
+        def __neg_sig_fn(x):
+            if len(x) == 1 and isinstance(x[0], sorts.BitvectorSort):
+                return sort_ctx.get_bv_sort(x[0].get_len())
+        neg_sig = synscope.FunctionSignature(__neg_sig_fn, 1, False)
+        target.add_signature("bvneg", neg_sig)
+        target.add_signature("bvnot", neg_sig)
+
+    @staticmethod
+    def __add_bv_binary_fns(target: synscope.SyntacticFunctionScope, sort_ctx: sorts.SortContext):
+        def __binary_sig_fn(x):
+            if len(x) == 2 and all(isinstance(z, sorts.BitvectorSort) for z in x) and x[0].get_len() == x[1].get_len():
+                return sort_ctx.get_bv_sort(x[0].get_len())
+        binary_sig = synscope.FunctionSignature(__binary_sig_fn, 2, False)
+
+        for name in ("bvand", "bvor", "bvadd", "bvmul", "bvudiv", "bvurem", "bvshl", "bvlshr"):
+            target.add_signature(name, binary_sig)
+
+    @staticmethod
+    def __add_comparison_fns(target: synscope.SyntacticFunctionScope, sort_ctx: sorts.SortContext):
+        def __comp_sig_fn(x):
+            if len(x) == 2 and all(isinstance(z, sorts.BitvectorSort) for z in x) and x[0].get_len() == x[1].get_len():
+                return sort_ctx.get_bool_sort()
+
+        target.add_signature("bvult", synscope.FunctionSignature(__comp_sig_fn, 2, False))
+
+    def create_syntactic_scope(self,
+                               sort_ctx: sorts.SortContext) -> synscope.SyntacticFunctionScope:
+        result = synscope.SyntacticFunctionScope(None)
+        self.__add_concat_fn(result, sort_ctx)
+        self.__add_extract_fn(result, sort_ctx)
+        self.__add_bv_neg_fns(result, sort_ctx)
+        self.__add_bv_binary_fns(result, sort_ctx)
+        self.__add_comparison_fns(result, sort_ctx)
+
+        return result
--- tests/examples/smt_qfbv_solver/unit/test_theories.py
+++ tests/examples/smt_qfbv_solver/unit/test_theories.py
@@ -1,6 +1,7 @@
 import unittest
 import examples.smt_qfbv_solver.theories as theories
 import examples.smt_qfbv_solver.sorts as sorts
+import examples.smt_qfbv_solver.syntactic_scope as synscope
 
 
 class TestCoreSyntacticFunctionScopeFactory(unittest.TestCase):
@@ -11,8 +12,8 @@ class TestCoreSyntacticFunctionScopeFactory(unittest.TestCase):
         sort_ctx = sorts.SortContext()
         scope = under_test.create_syntactic_scope(sort_ctx)
 
-        signature, name = scope.get_signature(name)
-        assert name == name
+        signature, lookup_name = scope.get_signature(name)
+        assert lookup_name == name
         assert signature.get_num_parameters() == 0
         assert signature.get_arity() == 2
 
@@ -67,8 +68,8 @@ class TestCoreSyntacticFunctionScopeFactory(unittest.TestCase):
         sort_ctx = sorts.SortContext()
         scope = under_test.create_syntactic_scope(sort_ctx)
 
-        signature, name = scope.get_signature(name)
-        assert name == name
+        signature, lookup_name = scope.get_signature(name)
+        assert lookup_name == name
         assert signature.get_num_parameters() == 0
         assert signature.get_arity() == 2
 
@@ -124,3 +125,187 @@ class TestCoreSyntacticFunctionScopeFactory(unittest.TestCase):
         assert signature.get_arity() == 0
         assert signature.get_num_parameters() == 0
         assert signature.get_range_sort([]) is sort_ctx.get_bool_sort()
+
+
+class TestFixedSizeBVSyntacticFunctionScopeFactory(unittest.TestCase):
+    def test_has_concat_fn(self):
+        under_test = theories.FixedSizeBVSyntacticFunctionScopeFactory()
+        sort_ctx = sorts.SortContext()
+        scope = under_test.create_syntactic_scope(sort_ctx)
+        signature, lookup_name = scope.get_signature("concat")
+
+        assert lookup_name == "concat"
+        assert signature.get_num_parameters() == 0
+        assert signature.get_arity() == 2
+
+        bv_sort_a = sort_ctx.get_bv_sort(2)
+        bv_sort_b = sort_ctx.get_bv_sort(4)
+        assert signature.get_range_sort([bv_sort_a, bv_sort_b]) is sort_ctx.get_bv_sort(6)
+
+    def test_concat_fn_is_only_applicable_to_bv_sorted_terms(self):
+        under_test = theories.FixedSizeBVSyntacticFunctionScopeFactory()
+        sort_ctx = sorts.SortContext()
+        scope = under_test.create_syntactic_scope(sort_ctx)
+        signature, _ = scope.get_signature("concat")
+
+        assert signature.get_range_sort([sort_ctx.get_bv_sort(0), sort_ctx.get_bool_sort()]) is None
+        assert signature.get_range_sort([sort_ctx.get_int_sort(), sort_ctx.get_bool_sort()]) is None
+
+    def test_has_extract_fn(self):
+        under_test = theories.FixedSizeBVSyntacticFunctionScopeFactory()
+        sort_ctx = sorts.SortContext()
+        scope = under_test.create_syntactic_scope(sort_ctx)
+        mangled_name = synscope.SyntacticFunctionScope.mangle_parametrized_function_name("extract")
+        signature, lookup_name = scope.get_signature(mangled_name)
+
+        assert lookup_name == mangled_name
+        assert signature.get_num_parameters() == 2
+        assert signature.get_arity() == 1
+
+        bv_sort = sort_ctx.get_bv_sort(10)
+        assert signature.get_range_sort([3, 1, bv_sort]) is sort_ctx.get_bv_sort(3)
+        assert signature.get_range_sort([0, 0, bv_sort]) is sort_ctx.get_bv_sort(1)
+        assert signature.get_range_sort([9, 9, bv_sort]) is sort_ctx.get_bv_sort(1)
+
+    def test_extract_fn_is_only_applicable_to_legally_sorted_terms(self):
+        under_test = theories.FixedSizeBVSyntacticFunctionScopeFactory()
+        sort_ctx = sorts.SortContext()
+        scope = under_test.create_syntactic_scope(sort_ctx)
+        mangled_name = synscope.SyntacticFunctionScope.mangle_parametrized_function_name("extract")
+        signature, _ = scope.get_signature(mangled_name)
+
+        assert signature.get_range_sort([3, 1, sort_ctx.get_int_sort()]) is None
+        assert signature.get_range_sort([3, 1, sort_ctx.get_bool_sort()]) is None
+        assert signature.get_range_sort([4, 4, sort_ctx.get_bv_sort(1)]) is None
+
+    @staticmethod
+    def __test_has_neg_fn(name):
+        under_test = theories.FixedSizeBVSyntacticFunctionScopeFactory()
+        sort_ctx = sorts.SortContext()
+        scope = under_test.create_syntactic_scope(sort_ctx)
+        signature, lookup_name = scope.get_signature(name)
+
+        assert lookup_name == name
+        assert signature.get_num_parameters() == 0
+        assert signature.get_arity() == 1
+
+        bv_sort = sort_ctx.get_bv_sort(3)
+        assert signature.get_range_sort([bv_sort]) is bv_sort
+
+    @staticmethod
+    def __test_neg_fn_is_only_applicable_to_bv_sorted_terms(name):
+        under_test = theories.FixedSizeBVSyntacticFunctionScopeFactory()
+        sort_ctx = sorts.SortContext()
+        scope = under_test.create_syntactic_scope(sort_ctx)
+        signature, _ = scope.get_signature(name)
+
+        assert signature.get_range_sort([sort_ctx.get_int_sort()]) is None
+        assert signature.get_range_sort([sort_ctx.get_bool_sort()]) is None
+
+    def test_has_bvneg_fn(self):
+        self.__test_has_neg_fn("bvneg")
+
+    def test_bvneg_fn_is_only_applicable_to_bv_sorted_terms(self):
+        self.__test_neg_fn_is_only_applicable_to_bv_sorted_terms("bvneg")
+
+    def test_has_bvnot_fn(self):
+        self.__test_has_neg_fn("bvnot")
+
+    def test_bvnot_fn_is_only_applicable_to_bv_sorted_terms(self):
+        self.__test_neg_fn_is_only_applicable_to_bv_sorted_terms("bvnot")
+
+    @staticmethod
+    def __test_has_binary_fn(name):
+        under_test = theories.FixedSizeBVSyntacticFunctionScopeFactory()
+        sort_ctx = sorts.SortContext()
+        scope = under_test.create_syntactic_scope(sort_ctx)
+        signature, lookup_name = scope.get_signature(name)
+
+        assert lookup_name == name
+        assert signature.get_num_parameters() == 0
+        assert signature.get_arity() == 2
+
+        bv_sort = sort_ctx.get_bv_sort(3)
+        assert signature.get_range_sort([bv_sort, bv_sort]) is bv_sort
+
+    @staticmethod
+    def __test_binary_fn_is_only_applicable_to_bv_sorted_terms(name):
+        under_test = theories.FixedSizeBVSyntacticFunctionScopeFactory()
+        sort_ctx = sorts.SortContext()
+        scope = under_test.create_syntactic_scope(sort_ctx)
+        signature, _ = scope.get_signature(name)
+
+        assert signature.get_range_sort([sort_ctx.get_int_sort(), sort_ctx.get_int_sort()]) is None
+        assert signature.get_range_sort([sort_ctx.get_bool_sort(), sort_ctx.get_int_sort()]) is None
+
+    def test_has_bvand_fn(self):
+        self.__test_has_binary_fn("bvand")
+
+    def test_bvand_fn_is_only_applicable_to_bv_sorted_terms(self):
+        self.__test_binary_fn_is_only_applicable_to_bv_sorted_terms("bvand")
+        
+    def test_has_bvor_fn(self):
+        self.__test_has_binary_fn("bvor")
+
+    def test_bvor_fn_is_only_applicable_to_bv_sorted_terms(self):
+        self.__test_binary_fn_is_only_applicable_to_bv_sorted_terms("bvor")
+        
+    def test_has_bvadd_fn(self):
+        self.__test_has_binary_fn("bvadd")
+
+    def test_bvadd_fn_is_only_applicable_to_bv_sorted_terms(self):
+        self.__test_binary_fn_is_only_applicable_to_bv_sorted_terms("bvadd")
+        
+    def test_has_bvmul_fn(self):
+        self.__test_has_binary_fn("bvmul")
+
+    def test_bvmul_fn_is_only_applicable_to_bv_sorted_terms(self):
+        self.__test_binary_fn_is_only_applicable_to_bv_sorted_terms("bvmul")
+        
+    def test_has_bvudiv_fn(self):
+        self.__test_has_binary_fn("bvudiv")
+
+    def test_bvudiv_fn_is_only_applicable_to_bv_sorted_terms(self):
+        self.__test_binary_fn_is_only_applicable_to_bv_sorted_terms("bvudiv")
+        
+    def test_has_bvurem_fn(self):
+        self.__test_has_binary_fn("bvurem")
+
+    def test_bvurem_fn_is_only_applicable_to_bv_sorted_terms(self):
+        self.__test_binary_fn_is_only_applicable_to_bv_sorted_terms("bvurem")
+        
+    def test_has_bvshl_fn(self):
+        self.__test_has_binary_fn("bvshl")
+
+    def test_bvshl_fn_is_only_applicable_to_bv_sorted_terms(self):
+        self.__test_binary_fn_is_only_applicable_to_bv_sorted_terms("bvshl")
+
+    def test_has_bvlshr_fn(self):
+        self.__test_has_binary_fn("bvlshr")
+
+    def test_bvlshr_fn_is_only_applicable_to_bv_sorted_terms(self):
+        self.__test_binary_fn_is_only_applicable_to_bv_sorted_terms("bvlshr")
+
+    def test_has_bvult_fn(self):
+        under_test = theories.FixedSizeBVSyntacticFunctionScopeFactory()
+        sort_ctx = sorts.SortContext()
+        scope = under_test.create_syntactic_scope(sort_ctx)
+        signature, lookup_name = scope.get_signature("bvult")
+
+        assert lookup_name == "bvult"
+        assert signature.get_num_parameters() == 0
+        assert signature.get_arity() == 2
+
+        bv_sort_a = sort_ctx.get_bv_sort(4)
+        bv_sort_b = sort_ctx.get_bv_sort(4)
+        assert signature.get_range_sort([bv_sort_a, bv_sort_b]) is sort_ctx.get_bool_sort()
+
+    def test_bvult_fn_is_only_applicable_to_bv_sorted_terms(self):
+        under_test = theories.FixedSizeBVSyntacticFunctionScopeFactory()
+        sort_ctx = sorts.SortContext()
+        scope = under_test.create_syntactic_scope(sort_ctx)
+        signature, _ = scope.get_signature("bvult")
+
+        assert signature.get_range_sort([sort_ctx.get_bv_sort(1), sort_ctx.get_bool_sort()]) is None
+        assert signature.get_range_sort([sort_ctx.get_bv_sort(1), sort_ctx.get_bv_sort(2)]) is None
+        assert signature.get_range_sort([sort_ctx.get_bool_sort(), sort_ctx.get_bool_sort()]) is None
