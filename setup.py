db084d9bc6cb02d368a8d0dd04a615afe5c82478
Felix Kutzner <felixkutzner@gmail.com>
2019-02-20 16:00:24 +0100

    ship tests and examples in future pycscl package

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from distutils.core import setup
+import setuptools
 
 setup(
     name='PyCSCL',
     version='0.1.0',
-    packages=['cscl',],
+    author='Felix Kutzner',
+    author_email='felixkutzner@gmail.com',
+    url='https://github.com/fkutzner/PyCSCL',
     license='MIT',
     long_description=open('README.md').read(),
+    packages=setuptools.find_packages()
 )
