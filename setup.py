590e17d515cedcfedaaa2af211d621b1a83744d9
Felix Kutzner <felixkutzner@gmail.com>
2019-02-24 16:48:11 +0100

    update package metadata in setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from distutils.core import setup
 import setuptools
 
 setup(
-    name='PyCSCL',
+    name='pycscl',
     version='0.1.0',
     author='Felix Kutzner',
     author_email='felixkutzner@gmail.com',
     url='https://github.com/fkutzner/PyCSCL',
     license='MIT',
     long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     packages=setuptools.find_packages()
 )
