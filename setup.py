9de36ff4ab136bbd09d531192f1b8f8d841f0255
Felix Kutzner <felixkutzner@gmail.com>
2020-02-05 16:50:34 +0100

    add description in setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     author_email='felixkutzner@gmail.com',
     url='https://github.com/fkutzner/PyCSCL',
     license='MIT',
+    description='Lightweight SAT Boolean constraint encoder library',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages()
